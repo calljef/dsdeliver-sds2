3136bb156962f0c822c3ff83e6ec0bd39b0e7237
calljef <jeferson.nascimento@yahoo.com.br>
2021-01-09 19:03:20 -0300

    Search order location

--- front-web/src/Orders/StepsHeader.tsx
+++ front-web/src/Orders/StepsHeader.tsx
@@ -5,11 +5,11 @@ function StepsHeader() {
         <h1 className="steps-title">SIGA AS <br/> ETAPAS</h1>
         <ul className="steps-items">
           <li>
-            <spa className="steps-number">1</spa>
+            <span className="steps-number">1</span>
             Selecione os produtos e a localização.
           </li>
           <li>
-          <spa className="steps-number">2</spa>
+          <span className="steps-number">2</span>
           Depois clique em  <strong>“ENVIAR PEDIDO”</strong> 
           </li>
         </ul>
